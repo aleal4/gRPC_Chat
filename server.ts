df85c06ef11bc5ccfad5b13f4d98c470ac1b6a17
Augusto Leal <aleal4@gmail.com>
2023-01-26 13:24:47 -0700

    server instatiated

--- server.ts
+++ server.ts
@@ -1,5 +1,38 @@
 import path from 'path'
 import * as grpc from '@grpc/grpc-js'
 import * as protoLoader from '@grpc/proto-loader'
+import {ProtoGrpcType} from './proto/random'
 
 const PORT = 8082
+const PROTO_FILE = './proto/random.proto'
+
+const packageDef = protoLoader.loadSync(path.resolve(__dirname, PROTO_FILE))
+const grpcObj = (grpc.loadPackageDefinition(packageDef) as unknown) as ProtoGrpcType
+const randomPackage = grpcObj.randomPackage
+
+function main () {
+  const server = getServer()
+
+  server.bindAsync(`0.0.0.0:${PORT}`, grpc.ServerCredentials.createInsecure(), 
+  (err, port) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log(`Your server has started on port ${port}`)
+    server.start()
+  })
+}
+
+function getServer(){
+  const server = new grpc.Server()
+  server.addService(randomPackage.Random.service, {
+    "PingPong": () => {
+
+    }
+  })
+
+  return server
+}
+
+main()
